2db1013a7c5312e54f13e1d6600868646d2f24e7
Ishan-ian12 <christianbataller788@gmail.com>
2025-06-03 16:00:56 +0800

    Add bataller profile.

---
+++ gwn10package/bataller.py
@@ -0,0 +1,6 @@
+def bataller_profile():
+    """ Display Christian Bataller's profile information. """
+    print("\nHello! This is Christian Bataller.")
+    print("1. I'm a cheerful, music-loving person who finds joy in singing.")
+    print("2. My goal is to achieve the goal I am currently pursuing.")
+    print("3. Hopefully, I can become a successful programmer in the future.")
