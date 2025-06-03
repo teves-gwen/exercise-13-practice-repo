f76d54e006910105bb629bdf786f83bf48d3213a
Gwen Teves <tevesgwenn.sch@gmail.com>
2025-06-03 15:38:33 +0800

    Add teves profile module and main menu. (#1)

---
+++ main.py
@@ -0,0 +1,21 @@
+from gwn10package.teves import teves_profile
+
+def main_menu():
+    """ Main program to display team member profiles. """
+    print("\n++ GWN10 Team members ++")
+    print("1. Gwen Teves")
+    print("0. Exit")
+
+    return input("\nEnter your choice: ").strip()
+
+while True:
+    choice = main_menu()
+
+    match choice:
+        case "1":
+            teves_profile()
+        case "0":
+            print("\nExiting the program...")
+            break
+        case _:
+            print("\nInvalid choice. Please try again.")
