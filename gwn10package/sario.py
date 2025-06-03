22526dd3021128619e1b8bdcc77593a36b2bcf8a
Gerald Sario <sariogrld02@gmail.com>
2025-06-03 16:17:36 +0800

    Add sario profile.

---
+++ gwn10package/sario.py
@@ -0,0 +1,6 @@
+def sario_profile():
+    """ Display Gerald Sario's profile information. """
+    print("\nHello Everyone! I'm Gerald Sario.")
+    print("1. I am a BSIT student from the PUP - Taguig.")
+    print("2. My goal is to become a become rich, richer, and richest.")
+    print("3. I want to become a Millionare, Billionare, Triollionare.")
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from gwn10package.teves import teves_profile
 from gwn10package.bataller import bataller_profile
+from gwn10package.sario import sario_profile
 
 def main_menu():
     """ Main program to display team member profiles. """
     print("\n++ GWN10 Team members ++")
     print("1. Gwen Teves")
     print("2. Christian Bataller")
+    print("3. Gerald Sario")
     print("0. Exit")
 
     return input("\nEnter your choice: ").strip()
@@ -18,6 +20,8 @@ while True:
             teves_profile()
         case "2":
             bataller_profile()
+        case "3":
+            sario_profile()
         case "0":
             print("\nExiting the program...")
             break
