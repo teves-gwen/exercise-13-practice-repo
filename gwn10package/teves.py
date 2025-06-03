c10c1150268321428ff88513e3c9dc7f757eeec8
teves-gwen <tevesgwenn.sch@gmail.com>
2025-06-03 15:25:17 +0800

    Add teves profile module and main menu.

---
+++ gwn10package/teves.py
@@ -0,0 +1,7 @@
+def teves_profile():
+    """ Display Gwen Teves' profile information. """
+    print("\nGood day, everyone! I'm Gwen Teves.")
+    print("1. I am now a 2nd year college student from BSIT.")
+    print("2. My goal is to graduate on time and live peacefully.")
+    print("3. I want to become an expert programmer.")
+
