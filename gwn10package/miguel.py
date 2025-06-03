2e19871f8f63363e910dfa79f8701923fa4b7121
bell.miguelkian@gmail.com <11bell.miguelkian@gmail.com>
2025-06-03 17:04:16 +0800

    Add miguel profile.

---
+++ gwn10package/miguel.py
@@ -0,0 +1,6 @@
+def miguel_profile():
+    """ Display Kian Miguel's profile information. """
+    print("\n======  Hello! This is Kian Miguel. ======")
+    print("1. I am a certified lover boy .")
+    print("2. My goal is to buy mclaren p1 and beat max verstappen in race.")
+    print("3. To be called Genius, billionaire, playboy, philanthropist")
