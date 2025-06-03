b5aa0c8ae4637897417e6cde50e79a37a765171b
Bataller, Christian <christianbataller788@gmail.com>
2025-06-03 16:07:49 +0800

    Add bataller profile. (#2)

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from gwn10package.teves import teves_profile
+from gwn10package.bataller import bataller_profile
 
 def main_menu():
     """ Main program to display team member profiles. """
     print("\n++ GWN10 Team members ++")
     print("1. Gwen Teves")
+    print("2. Christian Bataller")
     print("0. Exit")
 
     return input("\nEnter your choice: ").strip()
@@ -14,6 +16,8 @@ while True:
     match choice:
         case "1":
             teves_profile()
+        case "2":
+            bataller_profile()
         case "0":
             print("\nExiting the program...")
             break
